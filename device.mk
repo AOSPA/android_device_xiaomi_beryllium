ca1e085b1a10b2557dd76aabf5a3a27d9c95af3d
TheStrix <parthbhatia98@gmail.com>
2019-03-02 20:10:29 +0530

    beryllium: overlay-pa: Enable auto face unlock by default
    Change-Id: Ib9207ccbc4992452e5506aeeafe5f3394d5db230

--- device.mk
+++ device.mk
@@ -31,7 +31,8 @@ PRODUCT_COPY_FILES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-pa
 
 # Properties
 -include $(LOCAL_PATH)/device-props.mk
