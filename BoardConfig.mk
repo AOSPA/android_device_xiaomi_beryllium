d15784be22cfcf7177963f5f2ad5b91e6d40145a
Mesquita <mickaelmendes50@gmail.com>
2020-06-15 02:15:05 +0000

    beryllium: Set BOARD_VNDK_VERSION
    Change-Id: I54fd5e9efb9e10feef7bfc1f05e3a5058bdf7401

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -46,5 +46,8 @@ TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 # Recovery
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
 
+# Treble
+BOARD_VNDK_VERSION := current
+
 # Inherit from the proprietary version
 -include vendor/xiaomi/beryllium/BoardConfigVendor.mk
