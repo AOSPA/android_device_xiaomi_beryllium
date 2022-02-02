8273452b21fed5253865ff741c0a032f22974d4f
ZVNexus <akoskovich@pm.me>
2022-02-03 04:12:15 +0600

    beryllium: Build Paranoid Doze
    Change-Id: Idfe70fb3b3f66677c0b3ab9c6d19aec045ca41a0

--- device.mk
+++ device.mk
@@ -490,6 +490,13 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
+# ParanoidDoze
+PRODUCT_PACKAGES += \
+    ParanoidDoze
+
+PRODUCT_SYSTEM_EXT_PROPERTIES += \
+    ro.sensor.pickup=xiaomi.sensor.pickup
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
